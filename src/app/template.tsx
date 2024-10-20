70ef02b8ec1861fcd4c3c0b04336ed46b91c8de3
Kerry Creech <kerrywcreech@gmail.com>
2024-10-19 21:25:06 -0400

    Remove unnecessary code from template

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,23 +1,16 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { useState, useEffect } from "react"
 
 export default function Transition({
     children
 }: {
     children: React.ReactNode
 }) {
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() =>{
-        setIsMounted(true);
-    }, []);
-
     return (
         <motion.div
             initial={{ y: 20, opacity: 0 }}
-            animate={isMounted ? { y: 0, opacity: 1 } : {}}
+            animate={{ y: 0, opacity: 1 }}
             transition={{ ease: 'easeInOut', duration: 0.35 }}
         >
             {children}
